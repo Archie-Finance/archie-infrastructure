07d297158e7417401e09ceb6af7984b0c9826dd3
Luka <luka@archie.finance>
2022-10-09 23:06:46 +0200

    Added dynamodb

--- shared/terraform/modules/kubernetes/versions.tf
+++ shared/terraform/modules/kubernetes/versions.tf
@@ -1,12 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.4.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.8.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "4.4.0"
+#     }
+#     kubernetes = {
+#       source  = "hashicorp/kubernetes"
+#       version = "2.8.0"
+#     }
+#   }
+# }
